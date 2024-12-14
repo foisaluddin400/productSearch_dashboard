5215e115015ada73aeda211fe4869192911481e4
foisaluddin400 <foisalrk2@gmail.com>
2024-12-14 16:04:02 +0600

    updated

--- src/components/LayoutComponents/Header.jsx
+++ src/components/LayoutComponents/Header.jsx
@@ -224,7 +224,7 @@ const Header = () => {
                                   : "hover:bg-gray-200"
                               }`}
                               onClick={() => {
-                                setSelectedKey(child.key); // Set the selected key 
+                                setSelectedKey(child.key); // Set the selected 
                                 setExpandedKeys([]); // Collapse all expanded items
                                 onClose(); // Close the drawer after navigation
                               }}
