8c55bd21015da41bca2d0e69d0cbbf4c6d414764
foisaluddin400 <foisalrk2@gmail.com>
2024-12-14 16:02:12 +0600

    updated

--- src/components/LayoutComponents/Header.jsx
+++ src/components/LayoutComponents/Header.jsx
@@ -190,7 +190,7 @@ const Header = () => {
                           {item.label}
                         </span>
 
-                        {/* Show dropdown arrow  */}
+                        {/* Show dropdown   */}
                         {item.children && (
                           <FaChevronRight
                             className={`ml-auto transform transition-all duration-300 ${
