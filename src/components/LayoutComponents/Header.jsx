90691648fdd035c445ec029ac66c434875ba542c
foisaluddin400 <foisalrk2@gmail.com>
2024-12-14 16:02:31 +0600

    updated

--- src/components/LayoutComponents/Header.jsx
+++ src/components/LayoutComponents/Header.jsx
@@ -190,7 +190,7 @@ const Header = () => {
                           {item.label}
                         </span>
 
-                        {/* Sho    */}
+                        {/* Sh    */}
                         {item.children && (
                           <FaChevronRight
                             className={`ml-auto transform transition-all duration-300 ${
