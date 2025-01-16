3f6b22caa2baf6045ace456f0f7c3346e58d867d
unknown <foisalrk2@gmail.com>
2025-01-16 10:13:09 +0600

    update

--- src/page/Settings/Profile.jsx
+++ src/page/Settings/Profile.jsx
@@ -5,7 +5,7 @@ const Profile = () => {
 
   return (
     <div className="max-w-2xl mx-auto mt-8 bg-white rounded-lg p-6">
-      {/* Profile Picture Section */}
+    
       profile
     </div>
   );
