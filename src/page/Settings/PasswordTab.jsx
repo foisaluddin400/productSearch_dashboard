52a84906561470ae609e46632455d8fe591da723
foisaluddin400 <foisalrk2@gmail.com>
2025-06-14 14:24:24 +0600

    update

--- src/page/Settings/PasswordTab.jsx
+++ src/page/Settings/PasswordTab.jsx
@@ -33,7 +33,7 @@ export const PasswordTab = () => {
     //   message.error(error.data.message);
     // }
   };
-//adffadfasfdasdfasdf
+//adffadfasfdasdfasd
   return (
     <div>
       <Form layout="vertical" onFinish={handlePasswordChange}>
