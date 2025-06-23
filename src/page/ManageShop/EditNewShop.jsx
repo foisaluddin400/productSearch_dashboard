50ed1c89c9274374e6e4383ce80ca7d7c764dce2
foisaluddin400 <foisalrk2@gmail.com>
2025-06-23 14:46:35 +0600

    update

--- src/page/ManageShop/EditNewShop.jsx
+++ src/page/ManageShop/EditNewShop.jsx
@@ -175,7 +175,7 @@ const [shopAddress, setShopAddress] = useState("");
     loadShopData();
   }, [singleShopData, form]);
 
-//dsf
+//ds
 
   return (
     <Form form={form} layout="vertical" onFinish={onFinish}>
